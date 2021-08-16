6c81a8e4cadc5483f4c001196226cbfee52534eb
Jayesh Bhardwaj <jayeshb@amazon.com>
2021-08-16 23:19:12 +0530

    Improvements

--- src/json_to_csv/core.clj
+++ src/json_to_csv/core.clj
@@ -6,15 +6,18 @@
 
 
 (defn write-csv [path row-data]
-  (let [columns ["a" "b"]
-        headers (map name columns)
-        rows (mapv #(mapv % columns) row-data)]
     (with-open [file (io/writer path)]
-      (csv/write-csv file (cons headers rows)))))
+      (csv/write-csv file row-data)))
 
+
+(defn vectorize [cols map]
+  (mapv #(mapv % cols) map))
 
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (write-csv "/tmp/results.csv" (json/parse-string (slurp "/tmp/inp.json"))))
+  (let [[inp out] args
+        rows (json/parse-string (slurp inp))
+        cols (-> rows first keys)]
+    (write-csv out (cons cols (vectorize cols rows)))))
